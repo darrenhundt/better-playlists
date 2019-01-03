a742db1920f4a75d14e08df175c0f45f02d608df
Darren Hundt <darrenhundt@gmail.com>
2019-01-02 20:44:43 -0600

    duration data

--- src/App.js
+++ src/App.js
@@ -111,7 +111,8 @@ class App extends Component {
 //		}, 1000);
 		let parsed = queryString.parse(window.location.search);
 		let accessToken = parsed.access_token;
-		
+		if(!accessToken)
+			return;
 		fetch('https://api.spotify.com/v1/me', {
 			headers: {'Authorization': 'Bearer ' + accessToken}
 		}).then(response => response.json())
@@ -121,20 +122,48 @@ class App extends Component {
 				}
 		}))
 		
+		
+		
+		
 		fetch('https://api.spotify.com/v1/me/playlists', {
 			headers: {'Authorization': 'Bearer ' + accessToken}
 		}).then(response => response.json())
-			.then(data => this.setState({
-				playlists: data.items.map(item => {
+			.then(playlistData => {
+				let playlists = playlistData.items
+				let trackDataPromises = playlists.map(playlist => {
+					let responsePromise = fetch(playlist.tracks.href, {
+						headers: {'Authorization': 'Bearer ' + accessToken}
+					})
+					let trackDataPromise = responsePromise
+						.then(response => response.json())
+					return trackDataPromise
+				})
+				let allTracksDataPromises = 
+				Promise.all(trackDataPromises)
+				let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+					
+					trackDatas.forEach((trackData, i) => {
+						playlists[i].trackDatas = trackData.items
+							.map(item => item.track)
+							.map(trackData => ({
+								name: trackData.name,
+								duration: trackData.duration_ms / 60000
+							}))
+					})
+					return playlists
+				})
+				return playlistsPromise
+			})
+			.then(playlists => this.setState({
+				playlists: playlists.map(item => {
 					let smartUrl =  'https://www.fillmurray.com/g/160/160'
 					if (item.images[0]) {
 						smartUrl = item.images[0].url
 					}
-					console.log(smartUrl)
 					return {
 						name: item.name,
 						imageUrl: smartUrl,
-						songs: []
+						songs: item.trackDatas.slice(0,3)
 					}
 				})
 		}))
